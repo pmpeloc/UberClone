72718d3d724cec3a248ff4cd8e24371fa2c76f38
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-09-05 00:25:47 -0300

    feat: Google Autocomplete Places added

--- src/redux/slices/navSlice.ts
+++ src/redux/slices/navSlice.ts
@@ -1,15 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../store';
+import { Point } from 'react-native-google-places-autocomplete';
 
 export interface INavegation {
-  origin: string | null;
+  origin: Point | undefined;
   destination: string | null;
   travelTimeInformation: string | null;
 }
 
 const initialState: INavegation = {
-  origin: null,
+  origin: undefined,
   destination: null,
   travelTimeInformation: null,
 };
@@ -18,10 +19,13 @@ export const navSlice = createSlice({
   name: 'nav',
   initialState,
   reducers: {
-    setOrigin: (state, action: PayloadAction<string>) => {
+    setOrigin: (
+      state: INavegation,
+      action: PayloadAction<Point | undefined>,
+    ) => {
       state.origin = action.payload;
     },
-    setDestination: (state, action: PayloadAction<string>) => {
+    setDestination: (state, action: PayloadAction<string | null>) => {
       state.destination = action.payload;
     },
     setTravelTimeInformation: (state, action: PayloadAction<string>) => {
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, View, Image } from 'react-native';
+import {
+  GooglePlacesAutocomplete,
+  Point,
+} from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
 import tw from 'twrnc';
 
 import { NavOptions } from '../components/ui';
+import { setOrigin, setDestination } from '../redux/slices/navSlice';
 
 export const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={styles.logo}
           source={{ uri: 'https://links.papareact.com/gzs' }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details?.geometry.location,
+                description: data.description,
+              } as unknown as Point),
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'es',
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
         />
         <NavOptions />
       </View>
